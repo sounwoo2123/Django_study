2115fa88adaf98301580d9ecd695ef3356abf427
sounwoo2123 <123219997+sounwoo2123@users.noreply.github.com>
2025-06-04 20:22:49 +0900

    1234

--- back/locallibrary/locallibrary/settings.py
+++ back/locallibrary/locallibrary/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'catalog.apps.catalogConfig',
+    'catalog.apps.CatalogConfig',
 ]
 
 MIDDLEWARE = [
